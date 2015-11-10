50cdab063a771bbedcfd1a6a0d9c876791f9411e
Jared Armatis <jared.armatis@gmail.com>
2015-11-10 00:00:49 -0500

    Created Main Menu

---
+++ Fore Score and Seven Beers Ago/Assets/MainMenuScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public Button playButton;
+	public Button exitButton;
+
+	// Use this for initialization
+	void Start () {
+
+		playButton = playButton.GetComponent<Button> ();
+		exitButton = exitButton.GetComponent<Button> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame() {
+
+		Application.LoadLevel (1);
+
+	}
+
+	public void ExitGame() {
+
+		Application.Quit ();
+
+	}
+}
