9c39011fac2cba61e6749e4c954c9bc1b293cdb4
kylewild <kyleszalai@gmail.com>
2015-11-10 21:44:06 -0500

    Created more Prefabs and Scripts
    Prefabs created
    Tees (Plane temporarily)
    Pond (Plane)
    Gopher (Cube, potentially find a model like we did for the Alligator
    later?)
    
    Scripts created
    Gopher (Empty for now. Need to detect when player is near, but we have
    no player model to use right now)
    SandTrap (OnTriggerEnter method created, but empty due to no player
    model)
    Pond (Same as SandTrap)
    Tees (Same as SandTrap)
    --
    Kyle

---
+++ Fore Score and Seven Beers Ago/Assets/_Scripts/Pond.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pond : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        //When the trigger is entered
+        //Check if its the player
+        if (other.tag == "Player")
+        {
+            //Slow down player velocity
+            //We don't have an actual character model yet
+            //This will be handled once we have a character model
+            //It will limit player velocity
+            //This will behave similarly to SandTrap.cs?
+        }
+    }
+}
---
+++ Fore Score and Seven Beers Ago/Assets/_Scripts/SandTrap.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SandTrap : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+    {
+        //When the trigger is entered
+        //Check if its the player
+        if(other.tag == "Player")
+        {
+            //Slow down player velocity
+            //We don't have an actual character model yet
+            //This will be handled once we have a character model
+            //It will limit player velocity
+        }
+    }
+}
