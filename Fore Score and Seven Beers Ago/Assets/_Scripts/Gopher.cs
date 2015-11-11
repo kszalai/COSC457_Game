d8f8c3f4026cfa647cd784d0f6c87d2a8622634d
kylewild <kyleszalai@gmail.com>
2015-11-10 22:28:32 -0500

    Updated Gopher Prefab and Script
    Changed from Cube to Capsule

--- Fore Score and Seven Beers Ago/Assets/_Scripts/Gopher.cs
+++ Fore Score and Seven Beers Ago/Assets/_Scripts/Gopher.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class Gopher : MonoBehaviour {
 
+    //Seconds between Gopher pop ups
+    public float PopUpTime = 10f;
+
+    //Seconds Gopher remains above ground
+    public float AboveGroundTime = 2f;
+
 	// Use this for initialization
 	void Start () {
-	
+        InvokeRepeating("PopUpGopher", 2f, PopUpTime);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	IEnumerator PopUpGopher () {
+        transform.position = new Vector3(transform.position.x, 0.25f, transform.position.z);
+        yield return new WaitForSeconds(AboveGroundTime);
+        transform.position = new Vector3(transform.position.x, -0.5f, transform.position.z);
 	}
 }
