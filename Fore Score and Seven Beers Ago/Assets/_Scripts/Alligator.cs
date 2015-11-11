6bea1c4bcee20bb66bb70dd48d3ee73c2fdc5620
kylewild <kyleszalai@gmail.com>
2015-11-10 20:55:47 -0500

    Added Prefabs and Scripts
    New Prefabs
    CokeCan
    
    New Scripts
    CokeMachine
    CokeCan
    Alligator
    
    --
    Kyle

---
+++ Fore Score and Seven Beers Ago/Assets/_Scripts/Alligator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alligator : MonoBehaviour {
+
+    //Speed at which the Alligator Moves
+    public float speed = 1f;
+
+    //Distance where Alligator turns around
+    public float RightEdge = 10f;
+    public float LeftEdge = -10f;
+
+    void Start (){
+        RightEdge += transform.position.x;
+        LeftEdge += transform.position.x;
+    }
+	
+	void Update () {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        if(pos.x < LeftEdge){
+            speed = Mathf.Abs(speed); //Move right
+            transform.Rotate(0f, 180f, 0f);
+        }
+        else if(pos.x > RightEdge){
+            speed = -Mathf.Abs(speed); //Move left
+            transform.Rotate(0f, 180f, 0f);
+        }
+	}
+}
---
+++ Fore Score and Seven Beers Ago/Assets/_Scripts/CokeCan.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CokeCan : MonoBehaviour {
+    public static float maxLeftZ = 6.5f;
+    public static float maxRightZ = -1.5f;
+	
+	// Update the can called once per frame
+	void Update () {
+        if(transform.position.z < maxRightZ || transform.position.z > maxLeftZ)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
