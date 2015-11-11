eb5a615b64b48a2b3e1e656c733f4bccb8a5576b
kylewild <kyleszalai@gmail.com>
2015-11-11 15:04:14 -0500

    Fixed Gopher
    Previous method was weird
    This one works. Added flag for AboveGround. No longer will try to stay
    above ground for 2 seconds, it will be above ground for 5 seconds, and
    then hide for 5 seconds.

--- Fore Score and Seven Beers Ago/Assets/_Scripts/Gopher.cs
+++ Fore Score and Seven Beers Ago/Assets/_Scripts/Gopher.cs
@@ -4,19 +4,25 @@ using System.Collections;
 public class Gopher : MonoBehaviour {
 
     //Seconds between Gopher pop ups
-    public float PopUpTime = 10f;
+    public float PopUpTime = 5f;
 
     //Seconds Gopher remains above ground
     public float AboveGroundTime = 2f;
+
+    private bool AboveGround = true;
 
 	// Use this for initialization
 	void Start () {
         InvokeRepeating("PopUpGopher", 2f, PopUpTime);
 	}
 	
-	IEnumerator PopUpGopher () {
-        transform.position = new Vector3(transform.position.x, 0.25f, transform.position.z);
-        yield return new WaitForSeconds(AboveGroundTime);
-        transform.position = new Vector3(transform.position.x, -0.5f, transform.position.z);
-	}
+    void PopUpGopher () {
+        Vector3 pos = transform.position;
+        if(!AboveGround)
+            pos.y = 0.25f;
+        else
+            pos.y = -0.5f;
+        AboveGround = !AboveGround;
+        transform.position = pos;
+    }
 }
