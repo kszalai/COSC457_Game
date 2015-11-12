034c507f7ca8513b945b6227b4d672f847017a8a
hounvs <hounvs@gmail.com>
2015-11-12 13:30:02 -0500

    Fix Player hitbox, update drive code, add Alligator hitbox
    Now moves between lanes and drives more smoothly

--- Fore Score and Seven Beers Ago/Assets/_Scripts/Player.cs
+++ Fore Score and Seven Beers Ago/Assets/_Scripts/Player.cs
@@ -6,19 +6,42 @@ public class Player : MonoBehaviour {
     public float speed;
 
     private Rigidbody rb;
+    private int currentLane;
+    private int minLane;
+    private int maxLane;
+    private Vector3 movement;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        currentLane = 0;
+        minLane = 0;
+        maxLane = 5;
     }
 
-    void FixedUpdate()
+    void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (moveVertical >= .5 || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            changeLane(1);
+        }
+        else if (moveVertical <= -.5 || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            changeLane(-1);
+        }
+        else
+        {
+            //stay in same lane
+        }
 
+        movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+    }
+
+    void FixedUpdate()
+    {
         rb.AddForce(movement * speed);
     }
 
@@ -48,6 +71,20 @@ public class Player : MonoBehaviour {
             case "Tees":
                 print("Hit by Tees");
                 break;
+        }
+    }
+
+    private void changeLane(int direction)
+    {
+        if(direction == 1 && currentLane < maxLane)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+            currentLane++;
+        }
+        else if(direction == -1 && currentLane > minLane)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+            currentLane--;
         }
     }
 }
