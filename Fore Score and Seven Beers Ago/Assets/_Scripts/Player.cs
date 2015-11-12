52204edf5061aa3e4669d47b5eeb610fae6b902b
kylewild <kyleszalai@gmail.com>
2015-11-11 19:14:49 -0500

    Added Tags to Prefabs
    Added Tags to Prefabs and started script for player that will detect
    collisions with other objects

---
+++ Fore Score and Seven Beers Ago/Assets/_Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        string tag = other.gameObject.tag;
+        switch(tag)
+        {
+            case "Alligator":
+                print("Hit by Alligator");
+                break;
+            case "CokeCan":
+                print("Hit by CokeCan");
+                break;
+            case "CokeMachine":
+                print("Hit by CokeMachine");
+                break;
+            case "Gopher":
+                print("Hit by Gopher");
+                break;
+            case "Pond":
+                print("Hit by Pond");
+                break;
+            case "SandTrap":
+                print("Hit by SandTrap");
+                break;
+            case "Tees":
+                print("Hit by Tees");
+                break;
+        }
+    }
+}
