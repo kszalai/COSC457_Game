2b09d2b3322ef6f32ae1209e8fdadfaadcc492d3
Brianna Wurtsmith <bwoell@gmail.com>
2015-11-23 14:53:37 -0500

    Added Intro Scene, Intro Music, GUI Text, Started Script to Show Text
    Having roughly timed how long it would take a person to read through
    the paragraphs of text in the intro, I’ve created a song and added it
    to the Music Assets folder.
    
    I’ve also written out the script in several GUI text paragraphs, added
    it to the Intro Scene, and started a C# script that will turn each
    paragraph on/off after a certain number of seconds have elapsed.
    
    However, I have not yet connected the music or the C# script (the
    script is not yet working, so the non-working parts are commented out)

---
+++ Fore Score and Seven Beers Ago/Assets/_Scripts/IntroAdvanceText.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IntroAdvanceText : MonoBehaviour {
+
+	private Text paragraph1;
+	private Text paragraph2;
+	private Text paragraph3;
+	private Text paragraph4;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		paragraph1 = GetComponent<Text> ();
+		paragraph2 = GetComponent<Text> ();
+		paragraph3 = GetComponent<Text> ();
+		paragraph4 = GetComponent<Text> ();
+
+		//Set first paragraph to be visible, others to be invisible
+		paragraph2.enabled = false;
+		paragraph3.enabled = false;
+		paragraph4.enabled = false;
+		paragraph1.enabled = true;
+
+		//Allow 10 seconds to pass, then disable the current paragraph
+		//yield return new WaitForSeconds(10);
+		//paragraph1.enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+}
